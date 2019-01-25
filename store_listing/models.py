9064cd20ad0f6ff2cfb731ef2728bde4c1d7d304
= <ekirapapaul@gmail.com>
2019-01-26 01:56:40 +0300

    Add store listing models

--- store_listing/models.py
+++ store_listing/models.py
@@ -2,5 +2,33 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from colorfield.fields import ColorField
 # Create your models here.
+
+class Store(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    display_name = models.CharField(max_length=50)
+    image = models.ImageField(blank=True, null=True,upload_to=get_image_path)
+    website = models.CharField(null=True, blank=True, max_length=50)
+    telephone = models.CharField(null=True, blank=True, max_length=50)
+    email = models.EmailField(null=True, blank=True, max_length=50)
+    color = ColorField(default='#FFFFFF')
+
+    def __str__(self):
+    	return self.name
+
+class Outlet(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    display_name = models.CharField(blank=True, null=True, max_length=50)
+    latitude = models.DecimalField(max_digits=12, decimal_places=8, null=True)
+    longitude = models.DecimalField(max_digits=12, decimal_places=8, null=True)
+    working_hours = models.CharField(max_length=100, null=True)
+    image = models.ImageField(upload_to=get_image_path, null=True, blank=True)
+    rating = models.DecimalField(max_digits=2, decimal_places=1, null=True)
+    location = models.CharField(max_length=255, null=True)
+    telephone = models.CharField(max_length=30, null=True)
+    website = models.CharField(max_length=30, null=True)
+    store = models.ForeignKey(Store, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
