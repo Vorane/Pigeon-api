101ecf0175d98309b8c789041720a724ae240a9d
= <ekirapapaul@gmail.com>
2019-01-26 02:58:14 +0300

    [add] store listing serializers

--- store_listing/views.py
+++ store_listing/views.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, ListAPIView, RetrieveAPIView
+from store_listing.models import Store, Outlet
+from store_listing.serializers import OutletSerializer, StoreSerializer
+
 
 # Create your views here.
+class StoresView(ListCreateAPIView):
+    model = Store
+    querset = Store.objects.all()
+    serializer_class = StoreSerializer
+
+
+class OutletsView(ListCreateAPIView):
+    model = Outlet
+    querset = Outlet.objects.all()
+    serializer_class = OutletSerializer
