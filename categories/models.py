ac5ee3c066101f35fb7a4ac49d85e44a8712027f
Evans munene <munene.evansk@gmail.com>
2019-01-26 02:01:43 +0300

    [ADD] Catgory Model

--- categories/models.py
+++ categories/models.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from commons.models import BaseModel
 
 # Create your models here.
+class Category(BaseModel):
+	name = models.CharField(max_length=30, unique=False)
+	display_name = models.CharField(max_length=30, blank=True, null=True)
+	thumbnail = models.FileField(upload_to=get_image_path, null=True,  blank=True)
+	description = models.TextField(default="", null=True)
+	color = ColorField(default='#FFFFFF')
+
+	def __str__(self):
+		return self.name
