6b7f627c511ffcb4514dd0a70679739e0f0327a0
Evans munene <munene.evansk@gmail.com>
2019-01-26 11:54:46 +0300

    [ADD] models to admin site

--- product_listing/admin.py
+++ product_listing/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from product_listing.models import Product, SubCategoryProduct
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(SubCategoryProduct)
--- store_listing/admin.py
+++ store_listing/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from store_listing.models import Store, Outlet
 
 # Register your models here.
+admin.site.register(Store)
+admin.site.register(Outlet)
+
