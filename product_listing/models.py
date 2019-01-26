e27f6153c38fa9fd825a6a12e542ee6a16678b64
= <ekirapapaul@gmail.com>
2019-01-26 05:09:04 +0300

    [A]] product model

--- product_listing/models.py
+++ product_listing/models.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from commons.models import BaseModel
+from colorfield.fields import ColorField
+from commons.utils import get_image_path
 
 # Create your models here.
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=30, unique=False)
+    display_name = models.CharField(max_length=30, blank=True, null=True)
+    thumbnail = models.FileField(upload_to=get_image_path, null=True, blank=True)
+    description = models.TextField(default="", null=True)
+    color = ColorField(default='#FFFFFF')
+
+    def __str__(self):
+        return self.name
