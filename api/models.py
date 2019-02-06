60aafd701a8c119f4713ab835ec13df76f56a72d
= <ekirapapaul@gmail.com>
2019-02-07 00:13:04 +0300

    [ADD] mpesa

--- api/models.py
+++ api/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# Paynment transaction model
+class PaymentTransaction(models.Model):
+    phone_number = models.CharField(max_length=30)
+    amount = models.DecimalField(('amount'), max_digits=6, decimal_places=2, default=0)
+    isFinished = models.BooleanField(default=False)
+    isSuccessFull = models.BooleanField(default=False)
+    trans_id = models.CharField(max_length=30)
+    checkoutRequestID = models.CharField(max_length=100)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
