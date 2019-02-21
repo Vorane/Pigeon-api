dccbcf765884298d5b9a2ca2512696e9df570391
Evans munene <munene.evansk@gmail.com>
2019-02-21 15:52:01 +0300

    [UPDATE] statics

--- quicklane_api/settings.py
+++ quicklane_api/settings.py
@@ -123,3 +123,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# heroku deployment
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../static'),
+)
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
