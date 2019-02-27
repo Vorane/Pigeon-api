1d9035b6f22ec22f2e16c726c9415e867cdda8b7
= <ekirapapaul@gmail.com>
2019-02-27 12:39:39 +0300

    [ADD] mpesa views

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 from store_listing.urls import store_listing_urls
+from .views import ValidateView, ConfirmView, SubmitView
 
 app_name = "api"
 
 urlpatterns = [
     url(r'^storelisting/', include(store_listing_urls)),
-    
+    url(r'^validate/', ValidateView.as_view(), name='validate'),
+    url(r'^confirm/', ConfirmView.as_view(), name='confirm'),
+    url(r'^submit/', SubmitView.as_view(), name='submit'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,5 +2,93 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from .models import PaymentTransaction
+
 
 # Create your views here.
+class SubmitView(APIView):
+    permission_classes = [AllowAny,]
+
+    def post(self, request):
+        data = request.data
+        phone_number = data['phone_number']
+        amount = data['amount']
+        user_id = data['user']
+
+        print(phone_number)
+        print(amount)
+        sendSTK(phone_number, amount,user_id)
+        # b2c()
+        message = {"status": "ok"}
+        return Response(message, status=HTTP_200_OK)
+
+
+class ConfirmView(APIView):
+    permission_classes = [AllowAny,]
+
+    def post(self, request):
+        # save the data
+        request_data = json.dumps(request.data)
+        request_data = json.loads(request_data)
+        body = request_data.get('Body')
+        print("Body " + json.dumps(body))
+        resultcode = body.get('stkCallback').get('ResultCode')
+        # Perform your processing here e.g. print it out...
+        if resultcode == 0:
+            print('Payment successful')
+            requestId = body.get('stkCallback').get('CheckoutRequestID')
+            transaction =  PaymentTransaction.objects.filter(checkoutRequestID=requestId).get()
+            if transaction:
+                transaction.isFinished = True
+                transaction.isSuccessFull = True
+                transaction.save()
+
+        else:
+            print ('unsuccessfull')
+            requestId = body.get('stkCallback').get('CheckoutRequestID')
+            transaction = PaymentTransaction.objects.filter(checkoutRequestID=requestId).get()
+            if transaction:
+                transaction.isFinished = False
+                transaction.isSuccessFull = False
+                transaction.save()
+
+        # Prepare the response, assuming no errors have occurred. Any response
+        # other than a 0 (zero) for the 'ResultCode' during Validation only means
+        # an error occurred and the transaction is cancelled
+        message = {
+            "ResultCode": 0,
+            "ResultDesc": "The service was accepted successfully",
+            "ThirdPartyTransID": "1237867865"
+        }
+
+        # Send the response back to the server
+        return Response(message, status=HTTP_200_OK)
+
+    def get(self, request):
+        return Response("Confirm callback", status=HTTP_200_OK)
+
+
+class ValidateView(APIView):
+    permission_classes = [AllowAny,]
+
+    def post(self, request):
+        # save the data
+        request_data = request.data
+
+        # Perform your processing here e.g. print it out...
+        print("validate data" + request_data)
+
+        # Prepare the response, assuming no errors have occurred. Any response
+        # other than a 0 (zero) for the 'ResultCode' during Validation only means
+        # an error occurred and the transaction is cancelled
+        message = {
+            "ResultCode": 0,
+            "ResultDesc": "The service was accepted successfully",
+            "ThirdPartyTransID": "1234567890"
+        };
+
+        # Send the response back to the server
+        return Response(message, status=HTTP_200_OK)
