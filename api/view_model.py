f4cbe76090973887b98c9c61bf1d1e61307326b1
= <ekirapapaul@gmail.com>
2019-03-08 20:45:05 +0300

    [ADD] calculate amount on order

--- api/view_model.py
+++ api/view_model.py
@@ -1,4 +1,5 @@
-
+from .models import Wallet, PaymentTransaction
 
 def updateAvailableBalance(wallet_id, phone_number):
-    ''' Function to update available balance '''
+    """ Function to update available balance """
+    wallet = Wallet.objects.filter(wallet_id=wallet_id).get()
--- commons/utils.py
+++ commons/utils.py
@@ -1,6 +1,15 @@
 import uuid, os
 
+
 def get_image_path(instance, filename):
-	ext = filename.split('.')[-1]
-	filename = "%s.%s" % (uuid.uuid4(), ext)
-	return os.path.join(instance._meta.db_table, filename)
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join(instance._meta.db_table, filename)
+
+
+def validate_object(my_object, fields):
+    for field in fields:
+        if not field in my_object.keys():
+            return {"status": False, "field": field}
+    else:
+        return {"status": True}
