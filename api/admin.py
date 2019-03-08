945c41eedfe68ab50a47abf27c50ab28bc018b4d
= <ekirapapaul@gmail.com>
2019-03-08 19:09:39 +0300

    [ADD] register to admin

--- api/admin.py
+++ api/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Wallet, PaymentTransaction
 
 # Register your models here.
+admin.site.register(PaymentTransaction)
+admin.site.register(Wallet)
