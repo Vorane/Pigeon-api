cdd928dca2504e3939080ac6533a291a707e40d4
Evans <munene.evansk@gmail.com>
2019-04-13 19:34:29 +0300

    [UPDATE] subcategories in category details view

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,34 +1,52 @@
-from  rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer
 
 from store_listing.models import Store
 from .models import Category, SubCategory, CategorySubCategory
 
+
 class SubCategorySerializer(ModelSerializer):
     class Meta:
-        model= SubCategory
-        fields="__all__"
+        model = SubCategory
+        fields = "__all__"
+
 
 class CategorySerializer(ModelSerializer):
     class Meta:
-        model=Category
-        fields="__all__"
+        model = Category
+        fields = "__all__"
+
 
 class CategorySubCategorySerializer(ModelSerializer):
-    category=CategorySerializer()
-    sub_category= SubCategorySerializer()
+    category = CategorySerializer()
+    sub_category = SubCategorySerializer()
+
     class Meta:
-        model=CategorySubCategory
-        fields=("id","sub_category", "category")
+        model = CategorySubCategory
+        fields = ("id", "sub_category", "category")
 
 
 class CategorySubCategoryListingSerializer(ModelSerializer):
-    sub_categories = CategorySubCategorySerializer(many=True, source="category_categorysubcategory")
+    sub_categories = CategorySubCategorySerializer(
+        many=True, source="category_categorysubcategory")
+
     class Meta:
-        model=Category
-        fields=("id","name","sub_categories")
+        model = Category
+        fields = ("id", "name", "sub_categories")
+
+
+class CategoryDetailSerializer(ModelSerializer):
+    category_subcategories = CategorySubCategorySerializer(
+        many=True, source="category_categorysubcategory")
+
+    class Meta:
+        model = Category
+        fields = ("name", "display_name", "id", "category_subcategories",
+                  "thumbnail", "description", "color", "store")
+
 
 class StoreCategorySerializer(ModelSerializer):
-    categories = CategorySerializer(many=True, source="store_category")
+    categories = CategoryDetailSerializer(many=True, source="store_category")
+
     class Meta:
         model = Store
-        fields = ("id","name","categories")
+        fields = ("id", "name", "categories")
