5e12b37beb9f0a3c87a7af6eef5b694a87bf0026
Evans <munene.evansk@gmail.com>
2019-08-02 15:19:25 +0300

    [UPDATE] Staff orderSerializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,7 +3,7 @@ from rest_framework.serializers import ModelSerializer
 from store_listing.models import Outlet  #Restaurant
 from product_listing.models import Product
 from product_listing.serializers import ProductSerializer, ProductInventorySerializer  #ProductInlineSerializer
-from store_listing.serializers import OutletInlineSerializer  #RestaurantInlineSerializer
+from store_listing.serializers import OutletInlineSerializer, OutletSerializer  #RestaurantInlineSerializer
 from orders.models import Order, OrderItem
 from api.serializers import WalletInlineSerializer
 
@@ -17,6 +17,16 @@ class OrderInlineSerializer(ModelSerializer):
 class OrderItemInlineSerializer(ModelSerializer):
     # order = OrderInlineSerializer()
     product = ProductInventorySerializer()
+
+    class Meta:
+        model = OrderItem
+        fields = ("id", "order", "product", "quantity")
+
+
+#TODO swap out with orderItemInlineSerializer
+class OrderItemSerializer(ModelSerializer):
+    # order = OrderInlineSerializer()
+    product = ProductSerializer()
 
     class Meta:
         model = OrderItem
@@ -37,6 +47,18 @@ class OrderOrderItemSerializer(ModelSerializer):
 class OrderDetailSerializer(ModelSerializer):
     order_order_item = OrderItemInlineSerializer(many=True, read_only=True)
     outlet = OutletInlineSerializer(read_only=True)
+    wallet = WalletInlineSerializer(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ("id", "order_status", "comment", "pickup_time",
+                  "order_order_item", "outlet", "delivery_coordinates",
+                  "delivery_address", "order_contact_person", "wallet")
+
+
+class StaffOrderSerializer(ModelSerializer):
+    order_order_item = OrderItemSerializer(many=True, read_only=True)
+    outlet = OutletSerializer(read_only=True)
     wallet = WalletInlineSerializer(read_only=True)
 
     class Meta:
--- orders/staff_api.py
+++ orders/staff_api.py
@@ -3,13 +3,13 @@ from rest_framework.permissions import AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
 
 from orders.models import Order
-from orders.serializers import OrderInlineSerializer
+from orders.serializers import StaffOrderSerializer
 from orders.filters import OrderFilter
 
 
 class OrdersView(ListAPIView):
     permission_classes = [AllowAny]
-    serializer_class = OrderInlineSerializer
+    serializer_class = StaffOrderSerializer
     queryset = Order.objects.all()
     filter_backends = (DjangoFilterBackend, )
     filterset_class = OrderFilter
