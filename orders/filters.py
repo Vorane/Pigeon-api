e3b17cb7c2bbe450d087b7564bc3539c06ab3cee
Evans <munene.evansk@gmail.com>
2019-08-19 19:00:01 +0300

    [UPDATE] Orders filer accepts array of order status

--- orders/filters.py
+++ orders/filters.py
@@ -1,13 +1,20 @@
 from django_filters import rest_framework as filters
-from django_filters import DateTimeFilter, TimeRangeFilter, IsoDateTimeFilter, DateRangeFilter
+from django_filters import DateTimeFilter, TimeRangeFilter, IsoDateTimeFilter, DateRangeFilter, BaseInFilter, CharFilter
 
 from .models import Order
 
+
+
+class OrderStatusInFilter(BaseInFilter, CharFilter):
+    pass
+
 class OrderFilter(filters.FilterSet):
-    start_time = IsoDateTimeFilter(field_name='pickup_time',lookup_expr=('gt'),) 
+    start_time = IsoDateTimeFilter(field_name='pickup_time',lookup_expr=('gt'),)
     end_time = IsoDateTimeFilter(field_name='pickup_time',lookup_expr=('lt'))
-    
+    order_status = OrderStatusInFilter(field_name='order_status', lookup_expr='in')
 
     class Meta:
         model = Order
-        fields = ["pickup_time","order_status", "start_time", "end_time"]
+        fields = [
+            "pickup_time", "order_status", "start_time", "end_time",         
+        ]
